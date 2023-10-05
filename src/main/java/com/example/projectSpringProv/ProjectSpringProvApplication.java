d54dec262c2ea137b98706ae1434cf7eb7eb50bb
Noite <juliocesar1316@gmail.com>
2023-10-04 21:26:14 -0300

    first commit

---
+++ src/main/java/com/example/projectSpringProv/ProjectSpringProvApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectSpringProv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringProvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringProvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectSpringProv/ProjectSpringProvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectSpringProv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringProvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
